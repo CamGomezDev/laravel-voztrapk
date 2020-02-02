d6601e2723a9c604c9602f6f0633e16f6fe46cd9
dokasov <camilogozp@gmail.com>
2020-02-02 17:56:23 -0500

    Changed app name and implementation notes

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -12,7 +12,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+                {{ config('app.name', 'Voztra Politik') }}
             </a>
         </div>
 
