19f914879de90fb4cd81f2581e01fec4e6f682fa
dokasov <camilogozp@gmail.com>
2020-02-02 13:19:26 -0500

    database info for prod

--- config/database.php
+++ config/database.php
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'personal_heroku_mysql_connection'),
 
     /*
     |--------------------------------------------------------------------------
